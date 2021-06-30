0b212ad6beb2bf3d4adb85cd768c17d81f86f24a
sergiomartinnez <86322837+sergiomartinnez@users.noreply.github.com>
2021-06-30 15:39:32 -0500

    english v1

--- DataTypesJava/src/DoWhileLoop.java
+++ DataTypesJava/src/DoWhileLoop.java
@@ -4,17 +4,17 @@ public class DoWhileLoop {
     public static void main(String[] args) {
         int response = 0;
         do {
-            System.out.println("Selecciona el numero de la opcion deseada");
+            System.out.println("Choose the number of wished option");
             System.out.println("1 movies");
             System.out.println("2 series");
-            System.out.println("0 salir");
+            System.out.println("0 exit");
 
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
             switch (response) {
                 case 0:
-                    System.out.println("Gracias por visitarnos");
+                    System.out.println("thank you for visiting us");
                     break;
                 case 1:
                     System.out.println("Movies");
@@ -23,11 +23,11 @@ public class DoWhileLoop {
                     System.out.println("Series");
                     break;
                 default:
-                    System.out.println("selecciona una opcion correcta");
+                    System.out.println("choose a correct option");
             }
 
         }while (response != 0);
 
-        System.out.println("se termino el programa");
+        System.out.println("the program has ended");
     }
 }

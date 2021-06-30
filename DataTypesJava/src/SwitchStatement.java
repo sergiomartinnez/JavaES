9175a32820d0889ad7dd5853044a8bb1ba7c56f0
sergiomartinnez <86322837+sergiomartinnez@users.noreply.github.com>
2021-06-30 15:43:48 -0500

    english v1

--- DataTypesJava/src/SwitchStatement.java
+++ DataTypesJava/src/SwitchStatement.java
@@ -4,19 +4,19 @@ public class SwitchStatement {
         String colorModeSelected = "Dark";
         switch (colorModeSelected){
             case "Light":
-                System.out.println("Seleccionaste Light Mode");
+                System.out.println("choosen Light Mode");
                 break;
             case "Night":
-                System.out.println("Seleccionaste Night Mode");
+                System.out.println("choosen Night Mode");
                 break;
             case "Blue Dark":
-                System.out.println("Seleccionaste Blue dark Mode");
+                System.out.println("choosen Blue dark Mode");
                 break;
             case "Dark":
-                System.out.println("Seleccionaste Dark Mode");
+                System.out.println("choosen Dark Mode");
                 break;
             default:
-                System.out.println("Selecciona una opcion");
+                System.out.println("choose an option");
         }
 
     }
